8ca4ac18bafad1f66aab61add0528bbc446c96ec
Emmanuel Adegor <emmanueladegor2@gmail.com>
2017-07-01 21:56:32 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Sql Dump For List of All States in Nigeria and Their Respective
+
+ - Local Governments
+
+
+Just import to Your Sql Database to manage States and Local Governments
---
+++ state.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2017 at 10:53 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `richbest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `name`) VALUES
+(1, 'Abia State'),
+(2, 'Adamawa State'),
+(3, 'Akwa Ibom State'),
+(4, 'Anambra State'),
+(5, 'Bauchi State'),
+(6, 'Bayelsa State'),
+(7, 'Benue State'),
+(8, 'Borno State'),
+(9, 'Cross River State'),
+(10, 'Delta State'),
+(11, 'Ebonyi State'),
+(12, 'Edo State'),
+(13, 'Ekiti State'),
+(14, 'Enugu State'),
+(15, 'FCT'),
+(16, 'Gombe State'),
+(17, 'Imo State'),
+(18, 'Jigawa State'),
+(19, 'Kaduna State'),
+(20, 'Kano State'),
+(21, 'Katsina State'),
+(22, 'Kebbi State'),
+(23, 'Kogi State'),
+(24, 'Kwara State'),
+(25, 'Lagos State'),
+(26, 'Nasarawa State'),
+(27, 'Niger State'),
+(28, 'Ogun State'),
+(29, 'Ondo State'),
+(30, 'Osun State'),
+(31, 'Oyo State'),
+(32, 'Plateau State'),
+(33, 'Rivers State'),
+(34, 'Sokoto State'),
+(35, 'Taraba State'),
+(36, 'Yobe State'),
+(37, 'Zamfara State');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
